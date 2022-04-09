3f57d1ead07a011d3ee1b943f50a8183c98d896e
BK927 <dead4bees927@gmail.com>
2022-04-10 00:23:13 +0900

    update gem

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "제킬 마크다운 문법 샘플",
+        "excerpt":"Text can be bold, italic, strikethrough or keyword   Link to another page.   There should be whitespace between paragraphs.   There should be whitespace between paragraphs. We recommend including a README, or a file with information about your project.   Image   Images can be clicked to enlarge :) .      Header 1   This is a normal paragraph following a header. GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.   Header 2      This is a blockquote following a header.     When something is important enough, you do it even if the odds are not in your favor.    Header 3      This is another blockquote following header 3    // Javascript code with syntax highlighting. var fun = function lang(l) {   dateformat.i18n = require(\"./lang/\" + l);   return true; };   # Ruby code with syntax highlighting GitHubPages::Dependencies.gems.each do |gem, version|   s.add_dependency(gem, \"= #{version}\") end   Header 4      This is an unordered list following a header.   This is an unordered list following a header.   This is an unordered list following a header.   Header 5      This is an ordered list following a header.   This is an ordered list following a header.   This is an ordered list following a header.   Header 6                  head1       head two       three                       ok       good swedish fish       nice                 out of stock       good and plenty       nice                 ok       good oreos       hmm                 ok       good zoute drop       yumm           There’s a horizontal rule below this.     Here is an unordered list:      Item foo   Item bar   Item baz   Item zip   And an ordered list:      Item one            Item one       Item two                    Item one           Item two           Item three                       Item three           Item four   Item two   Item three   Item four   And a nested list:      level 1 item            level 2 item       level 2 item                    level 3 item           level 3 item                           level 1 item            level 2 item       level 2 item       level 2 item           level 1 item            level 2 item       level 2 item           level 1 item   Long, single-line code blocks should not wrap. They should horizontally scroll if they are too long Long, single-line code blocks should not wrap. They should horizontally scroll if they are too long.   The final element.  ","categories": ["내 생각"],
+        "tags": [],
+        "url": "/blog/%EB%82%B4%20%EC%83%9D%EA%B0%81/jekyll-markdown/",
+        "teaser": null
+      },{
+        "title": "이 블로그를 연 취지",
+        "excerpt":"이미 Gatsby로 만든 JAMSTACK 블로그 https://bustermachinelab.net/이 있다.   하지만 그럼에도 굳이 블로그를 만든 이유는 조금 더 개인적인 이유를 쓰기 위해서다.   BusterMachine Lab 정제되고, 어느 정도 수익을 목표를 글을 쓰기로 한다.   여기는 훨씬 더 개인적이고, 나 자신을 기록하고 목표를 달성하기 위해서 쓰는 것이다.   여기 글들은 훨씬 더 두서 없이 쓸 거고, SEO 따위는 신경도 안 쓸 거다.   확장성도 고려하지 않았기 때문에 Jekyll 기반으로 블로그를 만들었다.   여기까지 들어온 사람이라면 아마 나한테 관심이 많은 사람일 거다. 아니면 정말로 우연히 구글 검색에 걸렸거나. 혹은 Github 레파지토리를 유심히 보다가 왔을 지도 모르겠다.   이러니 저러니 해도 여기는 거의 숨김 없는 나의 모습일 것이다.   100% 나 자신을 위해서, 나 자신을 반성하기 위해서. 혹은 생각을 정리하기 위해서 글을 쓴다.   그래서 이 블로그의 이름의 달의 뒷면인 거다. 밝은 달의 뒷면은 흉하기 마련이니까.  ","categories": ["생각"],
+        "tags": [],
+        "url": "/blog/%EC%83%9D%EA%B0%81/%EC%9D%B4-%EB%B8%94%EB%A1%9C%EA%B7%B8%EB%A5%BC-%EC%97%B0-%EC%B7%A8%EC%A7%80/",
+        "teaser": null
+      },{
+        "title": "일지 템플릿",
+        "excerpt":"오늘의 목표      :white_check_mark:   오늘의 성취 인증         오늘의 피드백         어제의 나보다 나았던 점.         ","categories": ["일지"],
+        "tags": [],
+        "url": "/blog/%EC%9D%BC%EC%A7%80/%EC%9D%BC%EC%A7%80-%ED%85%9C%ED%94%8C%EB%A6%BF/",
+        "teaser": null
+      },{
+        "title": "카이스트 학생의 인출식 공부법",
+        "excerpt":"             나는 완벽주의 성향이 강해서 어떤 책이든 처음부터 씹어먹으려고 한다. 절대 사소한 것도 건너띄지 못하는 성격이다.   반면 이 사람은  사전처럼 참고한다고 한다. 아마 문제풀이, 혹은 아웃풋 위주로 공부를 하면서 막힐 때마다 책을 들여다보는 식인 거 같다.   인출 시도는 언제나 학습에 도움이 된다는 점을 생각해보면, 이건 지극히 과학적인 공부법이다.(심지어 학습하기 전에 인출하려고 시도하는 것도 도움이 된다는 연구 결과도 있다)   또 너무 깊이 들어가는 것을 방지하기 위해서 어느 정도만 깊이 들어가서 이해한다고 한다. 래빗홀에 빠지지 마라. 개발자들 사이에서도 자주 떠도는 이야기다.   나는 항상 근본 원리를 이해하지 않으면 넘어가지 않으려는 경향이 있다. 완벽주의와 합쳐져서 나타나는 끔찍한 부작용이다.   영상의 설명처럼 적당히 깊이 들어가고, 필요하면 나중에 공부하면 된다는 마인드가 중요한 거 같다.   극단적인 경우에도 2단계 초과해서 들어가는 것은 좋지 않은 것 같다.  ","categories": ["자기성찰"],
+        "tags": [],
+        "url": "/blog/%EC%9E%90%EA%B8%B0%EC%84%B1%EC%B0%B0/%EC%B9%B4%EC%9D%B4%EC%8A%A4%ED%8A%B8-%ED%95%99%EC%83%9D%EC%9D%98-%EC%9D%B8%EC%B6%9C%EC%8B%9D-%EA%B3%B5%EB%B6%80%EB%B2%95/",
+        "teaser": null
+      },{
+        "title": "마스터 듀얼을 플레이 하면서",
+        "excerpt":"      최근 마스터 듀얼이라는 게임에 빠졌다. 지금 이럴 시간이 없는데, 젊음의 시간은 소중한데 한 달에 200 시간이나 소모하다니 미친 거라는 생각 밖에 안 든다.   완전히 쓸데 없는 시간 낭비다. 그러나 그 와중에도 배운 게 있어서 여기에 써 본다.   나는 무엇을 하든 끈기가 없는 편이다. 충동적으로 일을 잘 벌리지만, 지속하지는 못한다. 성과가 나올 때까지 기다리지 못하는 천성탓일 거다.   그런 내가 마스터듀얼을 하면서 느낀 게 있다. 이 게임조차도, 마찬가지다. 처음하는 덱은 어떻게 돌아가는 건지 전혀 감이 잡히지 않는다.   나는 마기스토스라는 마이너한 덱을 플레이 했었는데, 사용자도 거의 없어서 어떤 식의 운영을 하는지 몰랐다. 전체적으로 카드 효과를 읽어보고, 실제로 덱을 운영하는 영상까지 봤지만 그래도 잘 모르겠었다.   그런데 막상 플레이하고, 몇번 꼬이고, 패배하고 하다보니까 어떤 식으로 굴려야 할지 금방 보이기 시작했다. 특별히 엄청난 노력을 한 것도 아니었다. 단순히 어느 순간 부터 이런 식으로 하면 좋겠다는 생각이 자연스럽게 들었다.   내가 생각했을 때는 배우는 과정도 그런 것 같다. 처음에는 아무리 책을 보고, 강의를 들어도 헤메고 고통 속에서 몸부림 치는게 정상이다. 그러나 어느 순간부터는 풀리게 된다. 그 과정은 선형적이지 않다. 어느 순간 갑작이 실력이 늘고, 정체한다. 마치 마기스토스 덱을 굴리는 것처럼 말이다.   여기서 생각이 드는 것은 나는 과연 다른 분야에서 이렇게 꾸준히 했는 가이다. 전부 조금 어려우면 포기하고 도망갔다. 내가 이해할 수 없는 단어를 보면 지레질색하면 도망치고, 도망치기의 연속이었다.   도대체 왜 그랬을까? 문턱의 높이 차이도 있지만, 결정적인 것은 자신감의 차이였던 거 같다. 마스터 듀얼에는 근거 없는 자신감이 있었다. 내가 아무리 어려워도 못 굴리는 덱이 있겠어? 라는 마음가짐이 존재했다. 실제로 나는 마기스토스 외에도 드래곤메이드나 블랙 매지션, 섀도르 처럼 컨트롤이 필수적이 덱을 굴리고 있다. 전부 난이도가 어렵다고 평을 듣지만, 나는 그래봐야 내가 못하겠어? 라는 심정으로 덤벼들었다. 그리고 실제로 일정 이상의 성과도 거뒀다.   앞으로 이러한 마인드를 가지고 하자. 처음에 안 되는 것은 당연한 것이다. 패배하고 고통스럽고, 도대체 일이 어떻게 돌아가는 건지 모르겠다면 그건 정상이다.   마스터 듀얼에 미쳐서 시간에 낭비했던 것처럼, 무언가에 몰입해서 미쳐보자. 부셔질 수 있는 시기는 어느 정도 한정적이다. 나는 지금 부서져야 할 시기다. 그것도 하루 빨리.  ","categories": ["생각"],
+        "tags": [],
+        "url": "/blog/%EC%83%9D%EA%B0%81/%EB%A7%88%EC%8A%A4%ED%84%B0-%EB%93%80%EC%96%BC%EC%9D%84-%ED%94%8C%EB%A0%88%EC%9D%B4-%ED%95%98%EB%A9%B4%EC%84%9C/",
+        "teaser": null
+      },{
+        "title": "2022/03/08 앞으로 계획",
+        "excerpt":"현재 내가 뻗어나갈 수 있는 선택지는 아래와 같다.                  할 수 있는 일       시간 제한       제 4분면 분류       비용                       떡 온라인 판매       5년~10년       4분면       시간: 약 3개월                 웹소설 집필       3년~5년       4분면       시간: ??                 데스티니 드로우 계산기       3 ~ 6개월 이내, 혹은 그보다 짧게 (마듀 인기 하락 전까지)       4분면       시간: 1주 ~ 1개월                 수익형 블로그 운영       무제한       4분면       사실 상 0 옵션 형태의 투자다                 개발자 취직       앞으로 1~2년 후 불가능       1분면       약 6개월의 집중 투자              떡 온라인 판매: 현재로서는 가장 합리적인 판단이라고 생각된다. 온라인 마켓팅을 공부하는데 다소 시간이 들겠지만, 후에 실패를 바탕삼아 다른 상품을 판매할 수도 있다. 사입이나 구매대행이 아니고 직접 제조한다는 점에서 더 좋다. 하지만 식품위생법등 규제를 지키기 좀 빡쎄다는 점이 문제다.   웹소설 집필: 가능성이 제일 낮은 길로 보인다. 기대수익이 그렇게 높은 것도 아니다. 그러나 나의 만족도와 자아실현에 영향을 제일 크게 준다. 그 동안 투자한 매몰비용 or 도피심리로 그렇게 느끼는 것일 수도 있다.   데스티니 드로우 계산기: 기대 수익율은 현재 산정이 불가능하다. 그러나 애드센스 게임 분야 비용을 비교해보면 낮지는 않을 것 같다. 다국어 지원을 통해 상단을 크게 열 수도 있다.   수익형 블로그 운영: 공부하는 거 정리 + 글쓰기 연습의 용도다. 수익을 내면 좋은 정도지, 수익을 못 내도 무관하다.   개발자 취직: 내가 생각했을 때 반드시 병용해야하는 것이다. 나의 계획 중에 유일하게 1분면에 포함된 길이다. 바벨 전략을 위해 무조건 해야 된다.   현재로서는 데스티니 드로우 계산기가 시간 제한이 가장 빡빡하고, 비용도 적기 때문에 일단 집중하도록 한다.   블로그는 계속 운영한다. 공부하는데 공책을 대신하는 것이다.   개발자 공부는 무조건 필수다. 설사 취직을 못하더라도 IT 기반 사업을 할 때 도움이 될 것이다.   루틴 계획        ","categories": ["계획설계"],
+        "tags": [],
+        "url": "/blog/%EA%B3%84%ED%9A%8D%EC%84%A4%EA%B3%84/%EC%95%9E%EC%9C%BC%EB%A1%9C-%EA%B3%84%ED%9A%8D/",
+        "teaser": null
+      },{
+        "title": "나는 원소형 사람이다.",
+        "excerpt":"                조조는 지금 할 수 있는 최선의 방법을 찾아요. 탁상에서 보면 60점 짜리에요. 근데 이게 지금 돌아가는 여러가지를 보면, 지금 이게 최선인 것이야. 조조는 그게 10점 짜리라도, 지금 이게 최선이라면 합니다. 근데 원소 같은 스타일은 이건 이런 문제가 있는데, 저건 저런 문제가 있는데… 회의하다가 날 다 새고, 드디어 만들면, 물 다 건너가는 경우가 많죠.    애자일(Agile), 린스타(lean start). 환경에 민감하게 반응하고, 변화하는 것은 강력한 경쟁력의 하나다. 행동력 하나만으로도 상당한 경쟁력을 갖출 수 있다.   위험을 인지하고 감수하는 것, 익숙하지 못한 환경에 용기를 내서 들어가는 것. 모든 성공한 영웅들의 특징이었다.   그렇다면 나는 어떨까? 겁쟁이다. 항상 그 자리에서 주변 자리를 살피고, 평가할 뿐. 나아갈 줄은 모르는 사람이다.   원소는 당대의 가장 큰 거물급 가문의 수장이었다. 그러나 그 끝은 비참했다. 그렇다면 나는 어떨까? 나는 원소 정도의 집안 배경도 없다. 그런데 원소처럼 행동한다면 어떻게 될까?   내게 필요한 건 베스트 플랜이 아니다. 린스타, 애자일 플랜이다. 뭐든지 당일날 시작하자. 그리고 실패하고 다시 계획을 세우자. 그게 지금 나 자신을 개선할 수 있는 방법이다.  ","categories": ["자기성찰"],
+        "tags": [],
+        "url": "/blog/%EC%9E%90%EA%B8%B0%EC%84%B1%EC%B0%B0/%EB%82%98%EB%8A%94-%EC%9B%90%EC%86%8C%ED%98%95-%EC%82%AC%EB%9E%8C%EC%9D%B4%EB%8B%A4/",
+        "teaser": null
+      },{
+        "title": "ffdgdgdg",
+        "excerpt":"   jiomnmoi  ","categories": ["일지"],
+        "tags": [],
+        "url": "/blog/%EC%9D%BC%EC%A7%80/ffdgdgdg/",
+        "teaser": null
+      }]
